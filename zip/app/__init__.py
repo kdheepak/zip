f3f7aae7fcb070caa49f6cdc462a27439862fe24
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-06-15 23:29:31 -0600

    Add freeze option

--- zip/app/__init__.py
+++ zip/app/__init__.py
@@ -1,8 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
+
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+
+app.config.from_pyfile('settings.py')
+
+pages = FlatPages(app)
+freezer = Freezer(app)
 
 from app import views
